98d06e57f8564cccc95765bd3478724232de7331
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-10-13 15:52:51 -0700

    Perform two basic simple existence tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
+// const App = () => <h1>Our First Test</h1>;
 
-const App = () => <h1>Our First Test</h1>;
+const App = () => {
+  return (
+    <section>
+      <h1 role="heading">Our First Test</h1>
+      <form action="/">
+        <label htmlFor="username">Username:</label>
+        <input role="usernameInput" type="text" name="username" />
+      </form>
+    </section>
+  );
+};
 
-export default App
+export default App;
--- tests/app.test.jsx
+++ tests/app.test.jsx
@@ -2,11 +2,19 @@ import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import App from "../src/App";
 
-
+import "@testing-library/jest-dom";
 
 describe("App component", () => {
   it("renders correct heading", () => {
     render(<App />);
     expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
   });
+
+  it("renders input field", () => {
+    render(<App />);
+    expect(screen.getByRole("usernameInput")).toBeInTheDocument();
+  });
+// Ok so we've successfully tested that these two elements exist.
+
+
 });
