93064637cf802cadd95ca5f5051b48b98cda2bd3
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-10-13 10:23:57 -0700

    Install testing dependencies and write first test

---
+++ src/greeting.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+
+function Greeting() {
+    return (
+        <div>
+            <h1> Hello there! </h1>
+        </div>
+    )
+}
+
+
+export default Greeting
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+import Greeting from './greeting.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Greeting />
   </StrictMode>,
 )
