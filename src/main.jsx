de1ae1c93c216c9ffdcd68242ecd4ee2d824631b
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-10-13 15:18:01 -0700

    Pass basic test

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx'
 import './index.css'
 
 
-import Greeting from './Greeting.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- tests/app.test.jsx
+++ tests/app.test.jsx
@@ -2,15 +2,11 @@ import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import App from "../src/App";
 
-import Greeting from "../src/Greeting"
 
-describe("App", () => {
-  it("renders headline", () => {
-    render(<App title="React" />);
 
-    screen.debug();
-    expect(screen.getAllByRole('heading').textContent).toMatch('Hello there!');
-    // check if App components renders headline
+describe("App component", () => {
+  it("renders correct heading", () => {
+    render(<App />);
+    expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
   });
 });
-
